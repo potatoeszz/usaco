5abfd1bd84de83068181a0e5e93b4b6ba35d0e4a
potatoeszz <boopioz101@gmail.com>
2024-11-10 14:08:45 -0500

    solved candy cane

---
+++ contest/2023Dec/candyCane.py
@@ -0,0 +1,34 @@
+N, M = [int(w) for w in input().strip().split()]
+cows = [int(w) for w in input().strip().split()]
+candies = [int(w) for w in input().strip().split()]
+
+#return how much the cow eats
+def eat(cowHeight, candyHeight, candyBase):
+    if cowHeight > candyBase:
+        if cowHeight < candyHeight:
+            return cowHeight - candyBase
+        else:
+            return candyHeight - candyBase
+    else:
+        return 0
+
+for i in range(M): # loop for candies
+    candyBase = 0
+    candyHeight = candies[i]
+    for j in range(N): # loop for cows
+        cowHeight = cows[j]
+        
+        amountEaten = 0
+        if cowHeight > candyBase:
+            if cowHeight < candyHeight:
+                amountEaten = cowHeight - candyBase
+            else:
+                amountEaten = candyHeight - candyBase
+
+        cows[j] += amountEaten
+        candyBase += amountEaten
+        if candyBase >= candyHeight:
+            break
+
+for cow in cows:
+    print(cow)
