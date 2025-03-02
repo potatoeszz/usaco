cb7c9619f6d9ea5293f11c5fa4e3307df6004eab
potatoeszz <boopioz101@gmail.com>
2025-03-02 16:16:16 -0500

    shorten code via import

--- contest/2024Dec/2.py
+++ contest/2024Dec/2.py
@@ -1,40 +1,33 @@
 #import sys
 #sys.stdin = open("2.in")
 
+from collections import defaultdict
+
 # read the input
 N, Q = [int(e) for e in input().strip().split()]
 
-xRow = {}
-yRow = {}
-zRow = {}
+xRow = defaultdict(int)
+yRow = defaultdict(int)
+zRow = defaultdict(int)
 
 ans = 0
 for _ in range(Q):
     x, y, z = [int(e) for e in input().strip().split()]
     # handle x row
     key = (y, z) # Tuple
-    if key in xRow:
-        xRow[key] += 1
-    else:
-        xRow[key] = 1
+    xRow[key] += 1
     if xRow[key] == N:
         ans += 1
 
     # handle y row
     key = (x, z)
-    if key in yRow:
-        yRow[key] += 1
-    else:
-        yRow[key] = 1
+    yRow[key] += 1
     if yRow[key] == N:
         ans += 1
 
     # hand z row
     key = (x, y)
-    if key in zRow:
-        zRow[key] += 1
-    else:
-        zRow[key] = 1
+    zRow[key] += 1
     if zRow[key] == N:
         ans += 1
 
